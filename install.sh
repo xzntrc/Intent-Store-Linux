a5125f7b9994ff72b2b6043485660e486900992c
Austin Eccentrici <96399844+xzntrc@users.noreply.github.com>
2022-07-09 12:20:08 +1000

    fixed stupid mistake

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-curl https://intent.store/jre/jre-8u51-linux-x64.tar.gz -o ~/Downloads/jreintent.tar.gz
-curl https://intent.store/intentlauncher/Intent%20Launcher.zip -o ~/IntentLauncher.zip
-unzip ~/IntentLauncher.zip
+baseuser=$(logname)
+curl https://intent.store/jre/jre-8u51-linux-x64.tar.gz -o /home/$baseuser/Downloads/jreintent.tar.gz
+curl https://intent.store/intentlauncher/Intent%20Launcher.zip -o /home/$baseuser/IntentLauncher.zip
+unzip /home/$baseuser/IntentLauncher.zip
 mkdir /opt/intent
 mv Intent\ Launcher*.jar /opt/intent
-tar xvf ~/Downloads/jreintent.tar.gz -C /opt
-sudo echo -e '#!/bin/sh\nexport PATH="/opt/jre1.8.0_51/bin:$PATH";java -jar /usr/share/"Intent Launcher*" "$@"' | sudo tee /usr/local/bin/intent
+tar xvf /home/$baseuser/Downloads/jreintent.tar.gz -C /opt
+sudo echo -e '#!/bin/sh\nexport PATH="/opt/jre1.8.0_51/bin:$PATH";java -jar /usr/share/"Intent Launcher"* "$@"' | sudo tee /usr/local/bin/intent
 chmod +x /usr/local/bin/intent
-sudo echo -e '[Desktop Entry]\nType=Application\nExec=intent\nTerminal=false\nHidden=false\nName=Intent Launcher\nIcon=terminal' | tee /home/$USER/.local/share/applications/intent.desktop
+sudo echo -e '[Desktop Entry]\nType=Application\nExec=intent\nTerminal=false\nHidden=false\nName=Intent Launcher\nIcon=terminal' | tee /home/$baseuser/.local/share/applications/intent.desktop
