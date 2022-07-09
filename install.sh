5be6393a29b7ba13abd567f0ac53ff35bfdf0ab5
Austin Eccentrici <96399844+xzntrc@users.noreply.github.com>
2022-07-09 12:21:28 +1000

    Remove sudo from within

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ unzip /home/$baseuser/IntentLauncher.zip
 mkdir /opt/intent
 mv Intent\ Launcher*.jar /opt/intent
 tar xvf /home/$baseuser/Downloads/jreintent.tar.gz -C /opt
-sudo echo -e '#!/bin/sh\nexport PATH="/opt/jre1.8.0_51/bin:$PATH";java -jar /usr/share/"Intent Launcher"* "$@"' | sudo tee /usr/local/bin/intent
+echo -e '#!/bin/sh\nexport PATH="/opt/jre1.8.0_51/bin:$PATH";java -jar /usr/share/"Intent Launcher"* "$@"' | tee /usr/local/bin/intent
 chmod +x /usr/local/bin/intent
-sudo echo -e '[Desktop Entry]\nType=Application\nExec=intent\nTerminal=false\nHidden=false\nName=Intent Launcher\nIcon=terminal' | tee /home/$baseuser/.local/share/applications/intent.desktop
+echo -e '[Desktop Entry]\nType=Application\nExec=intent\nTerminal=false\nHidden=false\nName=Intent Launcher\nIcon=terminal' | tee /home/$baseuser/.local/share/applications/intent.desktop
